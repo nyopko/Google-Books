58c9c768f4d85d82c8bb67fdf738c8986ae5553f
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-21 11:28:56 -0400

    Added demo text to jumbotron component, tested stylesheet

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return <div className="jumbotron">
+    <h1>Hello World!</h1>
+    
+    </div>;
+}
+
+export default Jumbotron;
